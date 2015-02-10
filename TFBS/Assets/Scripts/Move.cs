96baef699ea4d612e031b65a8fa2b7d4b49a9e93
Paultato <Paultato@users.noreply.github.com>
2015-02-10 21:10:58 +0100

    Movie Script + Collision
    Added a Move script to the sphere
    I did set the collision between the plane and the sphere so it appears
    to be a realistic sphere that we can move
    Need to fix the fact that you can't jump but just fly + Fix the
    possibility to go trough the plane

---
+++ TFBS/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    public float playerSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = new Vector3(0, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal")*playerSpeed*Time.deltaTime);
+        transform.Translate(Vector3.up * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
+	}
+}
