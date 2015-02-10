193284960b71a08d13ef813fa5a060d1d71a1ce4
Paultato <paulbanus@gmx.fr>
2015-02-10 16:38:08 +0100

    Visual Studio Setup

---
+++ TFBS/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("Hello TFBS!");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
