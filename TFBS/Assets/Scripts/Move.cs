f7ba6c14d2e7abc76fd3e7710d1716aa98383302
ThinkChaos <ThinkChaos@users.noreply.github.com>
2015-02-10 21:39:37 +0100

    Code Style

--- TFBS/Assets/Scripts/Move.cs
+++ TFBS/Assets/Scripts/Move.cs
@@ -5,16 +5,14 @@ public class Move : MonoBehaviour
 {
     public float playerSpeed = 5.0f;
 
-    // Use this for initialization
     void Start()
     {
         transform.position = new Vector3(0, 1, 0);
     }
 
-    // Update is called once per frame
-    void Update() 
+    void Update()
     {
-        transform.Translate(Vector3.right * Input.GetAxis("Horizontal")*playerSpeed*Time.deltaTime);
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
         transform.Translate(Vector3.up * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
-	}
+    }
 }
