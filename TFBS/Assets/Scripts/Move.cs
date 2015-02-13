f73b4390dad1964bc43d6695defe5ced342ed199
Paultato <paulbanus@gmx.fr>
2015-02-13 11:53:25 +0100

    Jump + Collision Update
    Made a basic jump function (need fix)
    made a basic collision script (need a big fix)
    I still have problem with the collision and gravity plz send help

--- TFBS/Assets/Scripts/Move.cs
+++ TFBS/Assets/Scripts/Move.cs
@@ -4,15 +4,29 @@ using System.Collections;
 public class Move : MonoBehaviour
 {
     public float playerSpeed = 5.0f;
+    public float jumpForce = 0.01f;
 
     void Start()
     {
         transform.position = new Vector3(0, 1, 0);
     }
 
+    void LateUpdate()
+    {
+        while (transform.position.y <= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+        }  
+    }
+
     void Update()
     {
         transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
         transform.Translate(Vector3.up * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + jumpForce, transform.position.z);
+        }
+        
     }
 }
