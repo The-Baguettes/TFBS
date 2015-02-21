ebb7672795071b738aad49134a29e31a9d39cfbd
ThinkChaos <ThinkChaos@users.noreply.github.com>
2015-02-21 14:20:01 +0100

    Tweaks to menus and fix music settings
    Moved backgrounds to canvas
    Adjusted font sizes and changed slider in the settings menu

--- TFBS/Assets/Scripts/UI/SettingsMenu.cs
+++ TFBS/Assets/Scripts/UI/SettingsMenu.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public void Awake()
+    {
+        if (SharedState.MusicSource != null)
+        {
+            Slider slider = FindObjectOfType<Slider>();
+            slider.value = SharedState.MusicSource.volume;
+        }
+    }
+
     public void Back()
     {
         Scene.MainMenu.Activate();
@@ -10,5 +20,10 @@ public class SettingsMenu : MonoBehaviour
     public void Controls()
     {
         // TODO
+    }
+
+    public void Music(float volume)
+    {
+        SharedState.MusicSource.volume = volume;
     }
 }
