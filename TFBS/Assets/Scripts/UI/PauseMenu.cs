4d6ed83185750351e46d6a9a0c63a7cfedf02203
mickael_au <mickmick93@users.noreply.github.com>
2015-02-22 21:42:28 +0100

    Pause Menu in Environment scene
    A pause menu option is added to the environment scene. The player can
    pause the game by pressing the key escape. Then you will have to choose
    between different options :
    1) Resume (you can resume the game by pressing the key Escape also
    2) Main Menu
    3) Settings
    4) Quit the game

---
+++ TFBS/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Canvas PauseCanvas; //Our scene canvas 
+
+	void Start () 
+    {
+        Screen.lockCursor = true;
+        Screen.showCursor = false; 
+	}
+
+    public void Settings()
+    {
+        Scene.SettingsMenu.Activate();
+    }
+
+    public void MainMenu()
+    {
+        Scene.MainMenu.Activate();
+    }
+
+    public void Quit()
+    {
+        if (Application.isEditor)
+            EditorApplication.isPlaying = false;
+        else
+            Application.Quit();
+    }
+	
+    public void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && PauseCanvas.enabled) // If the key echap is pressed then the following instruction is loaded
+        {
+            PauseCanvas.enabled = false;
+            Time.timeScale = 1; // Time scale won't working 
+            Screen.lockCursor = true;
+            Screen.showCursor = false; 
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseCanvas.enabled = true;
+            Time.timeScale = 0; // Time scale won't working 
+            Screen.lockCursor = false;
+            Screen.showCursor = true; 
+        }
+   	}
+
+    public void Unpaused_espace ()
+    {
+        if (PauseCanvas.enabled && Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseCanvas.enabled = false;
+            Time.timeScale = 1;
+            Screen.lockCursor = true;
+            Screen.showCursor = false; 
+        }
+    }
+
+    public void Unpaused ()
+    {
+        PauseCanvas.enabled = false;
+        Time.timeScale = 1;
+        Screen.lockCursor = true;
+        Screen.showCursor = false; 
+    }
+}
