63aa0821df909ba3670d8a033e60afd0f6adb585
vhulcroc <cai.richard@hotmail.fr>
2015-02-25 18:20:48 +0100

    Fullscreen mode added, resolution can be changed
    The two resolution available are 1920 x 1080 and 1024 x 768.

---
+++ TFBS/Assets/Scripts/UI/FullscreenOrNot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FullscreenOrNot : MonoBehaviour
+{
+    Text text;
+    public int width;
+    public int height;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        width = Screen.width;
+        height = Screen.height;
+    }
+    void Update()
+    {
+        width = Screen.width;
+        height = Screen.height;
+    }
+    public void Fullscreen()
+    {
+        if (!Screen.fullScreen)
+        {
+            Screen.SetResolution(width, height, true);
+            text.text = "Fullscreen";
+        }
+        else
+        {
+            Screen.SetResolution(width, height, false);
+            text.text = "Windowed";
+        }
+    }
+
+}
---
+++ TFBS/Assets/Scripts/UI/Resolution.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Resolution : MonoBehaviour
+{
+    Text text;
+    int width = Screen.width;
+    int height = Screen.height;
+    bool full;
+
+    void Start()
+    {
+        full = Screen.fullScreen;
+        text = GetComponent<Text>();
+        text.text = width + " x " + height;
+    }
+    void Update()
+    {
+        full = Screen.fullScreen;
+    }
+    public void changeRes()
+    {
+        if (text.text == "1024 x 768")
+        {
+            Screen.SetResolution(1920, 1080, full);
+            text.text = "1920 x 1080";
+        }
+        else
+        {
+            Screen.SetResolution(1024, 768, full);
+            text.text = "1024 x 768";
+        }
+    }
+}
