f10858d7f8bd7ad58577a0cb8dc78a698c1cbc51
ThinkChaos <ThinkChaos@users.noreply.github.com>
2015-02-26 16:29:08 +0100

    Fix DoorAnimation.cs

--- TFBS/Assets/Scripts/DoorAnimation.cs
+++ TFBS/Assets/Scripts/DoorAnimation.cs
@@ -1,47 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class DoorAnimation : MonoBehaviour {
-
-    // Smothly open a door
-float smooth = 2.0f;
-var DoorOpenAngle = 90.0;
-private bool open;
-private bool enter;
-
-private var defaultRot : Vector3;
-private var openRot : Vector3;
-
-function Start(){
-defaultRot = transform.eulerAngles;
-openRot = new Vector3 (defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
-}
-
-
-function Update ()
+public class DoorAnimation : MonoBehaviour
 {
-    if(open)
+    float smooth = 2.0f;
+    float DoorOpenAngle = 90.0f;
+
+    bool open;
+
+    Vector3 defaultRot;
+    Vector3 openRot;
+
+    public void Start()
     {
-
-        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
-
+        defaultRot = transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
     }
-    else
+
+    public void Update()
     {
-        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+        Vector3 rot = open ? openRot : defaultRot ;
+        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, rot, Time.deltaTime * smooth);
     }
 
-    if(Input.MouseClick)
+    void OnMouseEnter()
     {
         open = !open;
     }
-}
-
-    function OnGUI()
-        {
-           if(enter)
-            {
-                GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'F' to open the door");
-            }
-        }
 }
