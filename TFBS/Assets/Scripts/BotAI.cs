275f24415be5134f63738806038e38286a063baf
PAUL <paulbanus@gmx.fr>
2015-03-07 17:45:42 +0100

    NavMesh +Patrol

--- TFBS/Assets/Scripts/BotAI.cs
+++ TFBS/Assets/Scripts/BotAI.cs
@@ -1,47 +1,27 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class BotAI : MonoBehaviour
 {
-    float AIspeed = 3.5f;
-    float MaxDistance = 50f;
-    float MinDistance = 1f;
-    float AIrotate = 90f;
+    public Transform waypoint1;
+    public Transform waypoint2;
+    public Transform waypoint3;
 
-    Transform Leader;
-    bool AImoving = true;
+    int w;
+    Vector3[] waypoints;
 
     void Start()
     {
-        Leader = GameObject.FindWithTag(Tags.Player).transform;
+        waypoints = new Vector3[] {waypoint1.position, waypoint2.position ,waypoint3.position};
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = waypoints[0]; 
+        //Leader = GameObject.FindWithTag(Tags.Player).transform;
     }
 
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, Leader.position);
-
-        if (distance == MinDistance)
-            transform.LookAt(Leader);
-        else if (MinDistance < distance && distance <= MaxDistance)
-        {
-            transform.position += transform.forward * AIspeed * Time.deltaTime;
-            transform.LookAt(Leader);
-        }
-        else
-            StartCoroutine(Patrol());
-
-    }
-
-    IEnumerator Patrol()
-    {
-        if (AImoving)
-        {
-            transform.position += transform.forward * AIspeed * Time.deltaTime;
-            yield return new WaitForSeconds(1.5f);
-            AImoving = false;
-        }
-        transform.Rotate(0, AIrotate * Time.deltaTime, 0);
-        yield return new WaitForSeconds(1.5f);
-        AImoving = true;
+       if (Vector3.Distance(transform.position, waypoints[w]) < 0.2)
+           GetComponent<NavMeshAgent>().SetDestination(waypoints[w = (w + 1) % waypoints.Length]);
     }
 }
