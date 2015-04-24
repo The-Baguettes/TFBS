ceef34aaf3900f0afff2a0536e0f67e1476014f1
ThinkChaos <ThinkChaos@users.noreply.github.com>
2015-04-24 22:16:59 +0200

    Add NoHit tag
    Fixes bullets colliding with invisible objects such as AI vision spheres
    and door proximity detectors

--- TFBS/Assets/Scripts/Utilities/Tags.cs
+++ TFBS/Assets/Scripts/Utilities/Tags.cs
@@ -3,5 +3,6 @@
     public const string Bullet = "Bullet";
     public const string Enemy = "Enemy";
     public const string MainCamera = "MainCamera";
+    public const string NoHit = "NoHit";
     public const string Player = "Player";
 }
