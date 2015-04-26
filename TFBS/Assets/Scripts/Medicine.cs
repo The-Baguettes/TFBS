e26f17cd10bb291fb55a322a0ea689c7c5ca574b
mickael_au <mickmick93@users.noreply.github.com>
2015-04-26 18:18:52 +0200

    Medecine Add
    I create a script to generate random medecine at random position defined
    by different Gameobject. These object can rotate also. At the create the
    object has the tag "Medecine".
    
    To Do : Heal the player

---
+++ TFBS/Assets/Scripts/Medicine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Medicine : MonoBehaviour
+{
+    public GameObject Life1;
+    public GameObject Life2;
+    int random;
+    GameObject Cube;
+
+    void Start()
+    {
+        random = Random.Range(1, 3);    
+        Cube = Random_generate();
+        Cube.tag = "Medecine";
+    }
+
+    void rotation(GameObject cube) // Allow the cube to rotate 
+    {
+        cube.transform.Rotate(cube.transform.position * Time.deltaTime * 2);
+    }
+
+    GameObject Random_generate()
+    {            
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);        
+        if(random == 1)
+        {
+            cube.transform.position = Life1.transform.position;
+        }
+        if(random == 2)
+        {
+            cube.transform.position = Life2.transform.position;
+        }
+        return cube;
+    }
+
+   
+
+    void Update()
+    {
+        rotation(Cube);
+    }
+}
