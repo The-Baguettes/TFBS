9af86a1876a51652693f0e867b1d6df93dbf7eba
vhulcroc <cai.richard@hotmail.fr>
2015-05-02 13:40:59 +0200

    Elevator alert
    1/ A text is displayed when the player try to use the elevator to access
    the floor he already is.
    2/ When the player is dead he can't use the elevator.

--- TFBS/Assets/Scripts/UseElevator.cs
+++ TFBS/Assets/Scripts/UseElevator.cs
@@ -6,15 +6,24 @@ public class UseElevator : MonoBehaviour
 {
     bool contact;
     Canvas PauseCanvas;
+    Canvas GameOverCanvas;
     public Scene firstFloor;
     public Scene secondFloor;
+    bool display;
+    float displayCD;
+    string floor;
+    public float displayTime;
     //public Scene thirdFloor;
 
 
     void Start()
     {
+        floor = "";
+        displayCD = 0;
+        display = false;
         contact = false;
         PauseCanvas = GameObject.Find("PauseCanvas").GetComponent<Canvas>();
+        GameOverCanvas = GameObject.Find("GameOverCanvas").GetComponent<Canvas>();
     }
 
     void OnTriggerEnter(Collider other)
@@ -35,24 +44,52 @@ public class UseElevator : MonoBehaviour
 
     void OnGUI()
     {
-        if (!PauseCanvas.enabled)
+        if (!PauseCanvas.enabled || !GameOverCanvas.enabled)
         {
             if (contact)
             {
                 GUI.Label(new Rect((Screen.width * 3 / 4) - 25, Screen.height / 2 - 40, 250, 50), "Which floor do you want to go?");
-                if (GUI.Button(new Rect((Screen.width * 3 / 4) - 25, Screen.height / 2, 150, 50), "1st Floor") && Application.loadedLevel != 4)
+                if (GUI.Button(new Rect((Screen.width * 3 / 4) - 25, Screen.height / 2, 150, 50), "1st Floor"))
                 {
-                    SceneManager.LoadScene(firstFloor);
+                    if (Application.loadedLevel != 4)
+                    {
+                        SceneManager.LoadScene(firstFloor);
+                    }
+                    else
+                    {
+                        display = true;
+                        floor = "1st";
+                        displayCD = Time.time;
+                    }
                 }
-                if (GUI.Button(new Rect((Screen.width * 3 / 4) - 25, Screen.height / 2 + 60, 150, 50), "2nd Floor") && Application.loadedLevel != 5)
+                if (GUI.Button(new Rect((Screen.width * 3 / 4) - 25, Screen.height / 2 + 60, 150, 50), "2nd Floor"))
                 {
-                    SceneManager.LoadScene(secondFloor);
+                    if (Application.loadedLevel != 5)
+                    {
+                        SceneManager.LoadScene(secondFloor);
+                    }
+                    else
+                    {
+                        display = true;
+                        floor = "2nd";
+                        displayCD = Time.time;
+                    }
                 }
                 /*
                 if (GUI.Button(new Rect((Screen.width * 3 / 4) - 25, Screen.height / 2 + 120, 150, 50), "3rd Floor") && EditorApplication.currentScene != thirdFloor.ToString())
                 {
                     SceneManager.LoadScene(thirdFloor);
                 }*/
+
+                //display alert part
+                if (display)
+                {
+                    GUI.Label(new Rect((Screen.width / 2) - 150, Screen.height / 2 - 50, 250, 200), "You already are in " + floor + " Floor");
+                }
+                if (displayCD + displayTime < Time.time)
+                {
+                    display = false;
+                }
             }
         }
     }
