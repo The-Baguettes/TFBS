8cc3852a4c512afc1e4bb255ef1e5f99df214328
PAUL <paulbanus@gmx.fr>
2015-05-02 21:51:05 +0200

    Added Checkpoint to floor1(read info)
    Added a checkpoint that says kill all the ennemies but I set it in front
    of the spawn to test it and you pass the level when you walk through the
    checkpoint
    to do:fix the objective

---
+++ TFBS/Assets/Scripts/Floor1Objective.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor1Objective : MonoBehaviour {
+
+    HUD hud;
+    void Start()
+    {
+        hud = GameObject.FindObjectOfType<HUD>();
+        hud.Objective = "Eliminates all the ennemies";
+    }
+    void OnTriggerEnter(Collider checkpointcol)
+    {
+        //print(checkpointcol.tag);
+        if (checkpointcol.tag == Tags.Player)
+        {
+            hud.Objective = "You Passed The Level";
+        }
+    }
+
+}
