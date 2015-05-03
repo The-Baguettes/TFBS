2dbf315faf21c99abf29ab251cb057e91b93cd00
vhulcroc <cai.richard@hotmail.fr>
2015-05-03 20:06:51 +0200

    Script fixes

--- TFBS/Assets/Scripts/Elevator/UseElevator.cs
+++ TFBS/Assets/Scripts/Elevator/UseElevator.cs
@@ -32,7 +32,7 @@ public class UseElevator : MonoBehaviour
         isClose = true;
         player = GameObject.FindWithTag(Tags.Player);
 
-        if (SceneManager.PreviousScene() != Scene.MainMenu || SceneManager.PreviousScene() != Scene.SettingsMenu)
+        if (SceneManager.PreviousScene() != Scene.MainMenu)
         {
             if (SceneManager.LoadedScene == Scene.Game)
             {
