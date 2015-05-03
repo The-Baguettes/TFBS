80593a61b15098de3b56acf20933c1eaa9efe8f4
ThinkChaos <ThinkChaos@users.noreply.github.com>
2015-05-03 17:41:48 +0200

    Temporary fix for ammo drops to update HUD

--- TFBS/Assets/Scripts/Drops/AmmoDrop.cs
+++ TFBS/Assets/Scripts/Drops/AmmoDrop.cs
@@ -4,12 +4,20 @@ public class AmmoDrop : DropBase
 {
     override protected Color32 color { get { return Color.black; } }
 
+    HUD hud;
+
+    void Start()
+    {
+        hud = GameObject.FindObjectOfType<HUD>();
+    }
+
     override protected bool Pickup()
     {
         if (weaponManager.ActiveGun == null)
             return false;
 
         weaponManager.ActiveGun.MagazineCount += 1;
+        hud.playerWeaponManager_OnUseActive();
         return true;
     }
 }
