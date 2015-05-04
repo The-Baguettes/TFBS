55128c0baef6e311666a672919c471592e346a13
vhulcroc <cai.richard@hotmail.fr>
2015-05-04 21:34:50 +0200

    Mission three

--- TFBS/Assets/Scripts/Objectives/GameObjective.cs
+++ TFBS/Assets/Scripts/Objectives/GameObjective.cs
@@ -6,13 +6,46 @@ public class GameObjective : MonoBehaviour
 
     void Start()
     {
+        //load from the editor
+        if (SecretBaseExitDoor.objective == 0)
+        {
+            SecretBaseExitDoor.objective = 3;
+        }
+
         hud = GameObject.FindObjectOfType<HUD>();
-        hud.SetObjective("Find the elevator");
+        if (SecretBaseExitDoor.objective == 1)
+        {
+            hud.SetObjective("Kill the enemies");
+        }
+        else
+            if (SecretBaseExitDoor.objective == 2)
+            {
+                hud.SetObjective("Kill the enemies");
+            }
+            else
+                if (SecretBaseExitDoor.objective == 3)
+                {
+                    hud.SetObjective("Find the confidential information in the secret room");
+                }
+                else
+                {
+                    hud.SetObjective(null);
+                }
+        //hud.SetObjective("Find the elevator");
     }
 
+    /*
     void OnTriggerEnter(Collider checkpointcol)
     {
         if (checkpointcol.tag == Tags.Player)
             hud.SetObjective(null);
+    }*/
+
+    void Update()
+    {
+        if (MissionComplete.missionCompleted)
+        {
+            hud.SetObjective("Mission completed \nfind a way to escape");
+        }
     }
 }
