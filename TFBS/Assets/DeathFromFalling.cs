d5c7c65a6fe9c62424cb28b727e0ee0f6a6ecd22
vhulcroc <cai.richard@hotmail.fr>
2015-05-05 00:15:48 +0200

    Game Limits and death from falling added

---
+++ TFBS/Assets/DeathFromFalling.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathFromFalling : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            //kill the player
+        }
+    }
+}
