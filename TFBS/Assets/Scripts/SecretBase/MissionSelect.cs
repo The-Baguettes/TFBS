c649f5004800e8c4d7f1ad8984b574ac6558c493
vhulcroc <cai.richard@hotmail.fr>
2015-05-04 16:52:27 +0200

    Some script for the secret base scene

---
+++ TFBS/Assets/Scripts/SecretBase/MissionSelect.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissionSelect : MonoBehaviour
+{
+    bool contact;
+    Canvas missionSelect;
+
+    void Start()
+    {
+        contact = false;
+        missionSelect = GameObject.Find("MissionSelect").GetComponent<Canvas>();
+        missionSelect.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            contact = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            contact = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (contact)
+        {
+            if (missionSelect.enabled)
+            {
+                if (GUI.Button(new Rect(Screen.width -200, Screen.height - 100, 100, 50), "Close", "button"))
+                {
+                    missionSelect.enabled = false;
+                }
+            }
+            else
+            {
+                GUI.Label(new Rect((Screen.width / 2) + 500, Screen.height / 2 - 50, 250, 200), "Press 'F' to open the mission selection window");
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (contact && Input.GetKeyDown(KeyCode.F))
+        {
+            missionSelect.enabled = !missionSelect.enabled;
+        }
+    }
+}
---
+++ TFBS/Assets/SecretBaseExitDoor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretBaseExitDoor : MonoBehaviour
+{
+    [HideInInspector]
+    public bool missionSelected;
+    [HideInInspector]
+    public int mission;
+
+    bool contact;
+    bool display;
+    public float displayTime;
+    float time;
+
+    void Start()
+    {
+        missionSelected = false;
+        contact = false;
+        display = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            contact = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            contact = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (contact)
+        {
+            if (missionSelected)
+            {
+                SceneManager.LoadScene((Scene)mission);
+            }
+            else
+            {
+                display = true;
+                time = Time.time;
+            }
+        }
+        if (display)
+        {
+            GUI.Label(new Rect((Screen.width / 2) + 500, Screen.height / 2 - 50, 250, 200), "You still haven't selected a mission");
+        }
+        if (time + displayTime < Time.time)
+        {
+            display = false;
+        }
+    }
+}
