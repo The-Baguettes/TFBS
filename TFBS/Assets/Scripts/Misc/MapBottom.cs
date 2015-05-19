25c4bbb7a73f35598843a32e2a6009f664d963be
ThinkChaos <ThinkChaos@users.noreply.github.com>
2015-05-19 20:07:52 +0200

    Destroy any object that falls below the level

---
+++ TFBS/Assets/Scripts/Misc/MapBottom.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MapBottom : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        BaseDamageable damageable = col.GetComponent<BaseDamageable>();
+
+        if (damageable != null)
+            damageable.Kill();
+
+        Destroy(col.gameObject);
+    }
+}
