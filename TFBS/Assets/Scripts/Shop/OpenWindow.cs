1079ab95c9307cd771d697184da8808b09c9c859
vhulcroc <cai.richard@hotmail.fr>
2015-06-18 00:03:59 +0200

    Shop windows added

---
+++ TFBS/Assets/Scripts/Shop/OpenWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OpenWindow : MonoBehaviour
+{
+    public Canvas canvas;
+
+    void Start()
+    {
+        canvas.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            canvas.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            canvas.enabled = false;
+        }
+    }
+}
