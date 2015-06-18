2541702a8e5966a61b932442c087c506f433f0e4
mickael_au <mickmick93@users.noreply.github.com>
2015-06-19 00:26:45 +0200

    Surveillance camera added

--- TFBS/Assets/Scripts/SCamera.cs
+++ TFBS/Assets/Scripts/SCamera.cs
@@ -3,89 +3,107 @@ using System.Collections;
 
 public class SCamera : MonoBehaviour
 {
-    #region Coordinates
-    public float XOffsetFromCenter;
-    public float YOffsetFromCenter;
-    public float ZOffsetFromCenter;
-    #endregion
     public bool isPlayer;
-    Vector3 positionUp;
-    Vector3 positionDown;
+    Vector3 minAngle;
+    Vector3 maxAngle;
+    Vector3 targetAngle;
+    public float toRotate;
     public float smooth;
-    public GameObject ToMove;
+    float animationStartTime;
+    bool vrai;
     bool final;
     float animationProgress = 0.0f;
-
-	void Start () {
-        GameObject.FindGameObjectWithTag("MainCamera").SetActive(false);
-        positionUp = ToMove.transform.position;
-        positionDown = new Vector3(positionUp.x + XOffsetFromCenter, positionUp.y + YOffsetFromCenter, positionUp.z + ZOffsetFromCenter);
-        final = false;        
-	}
-
-    void OnTriggerEnter(Collider col)
+    
+    void Start()
     {
-        if (col.tag == Tags.Player)
-            isPlayer = true;
+        minAngle = transform.eulerAngles + new Vector3(0,  - toRotate / 2, 0);
+        maxAngle = transform.eulerAngles + new Vector3(0, +toRotate / 2, 0);
+        targetAngle = minAngle;
+
     }
 
-    void OnTriggerExit(Collider col)
+    void OnTriggerStay(Collider col)
     {
         if (col.tag == Tags.Player)
         {
-            isPlayer = false;
+            
+            //TO DO DETECT PLAYER
         }
     }
 
+    
     IEnumerator Displacement()
     {
-            yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(5f);
     }
 
 
-
-	void Update () {
-
-        if (final)
+    void Update() 
+    {
+        if(Time.time <= animationStartTime)
         {
-            if (ToMove.transform.position == positionDown)
-            {
-                final = !final;
-            }
+            return;
+        }
+        animationProgress += Time.deltaTime*smooth;
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngle,animationProgress);
+        if(animationProgress >= 1)
+        {
+            animationProgress = 0;
+            animationStartTime = Time.time + 5f;
+            if (targetAngle == minAngle)
+                targetAngle = maxAngle;
             else
-            {
-                if (ToMove.transform.position == positionUp)
+                targetAngle = minAngle;
+        }
+    }
+    /*void Update()
+    {
+                if (final)
                 {
-                    StartCoroutine(Displacement());
-                    animationProgress = 0.0f;
-                    ToMove.transform.position = new Vector3(ToMove.transform.position.x - 0.0001f, ToMove.transform.position.y - 0.0001f, ToMove.transform.position.z - 0.0001f);
+                    if (ToMove.transform.position == positionDown)
+                    {
+                        final = !final;
+                        //vrai = !vrai;
+                    }
+                    else
+                    {
+                        if (ToMove.transform.position == positionUp && vrai)
+                        {
+                            StartCoroutine(Displacement());
+                            animationProgress = 0.0f;
+                            vrai = !vrai;
+                            ToMove.transform.position = new Vector3(ToMove.transform.position.x - 0.0001f, ToMove.transform.position.y - 0.0001f, ToMove.transform.position.z - 0.0001f);
+                        }
+                        else
+                        {
+                            ToMove.transform.position = Vector3.Lerp(positionUp, positionDown, animationProgress / smooth);
+                        }
+                    }
                 }
                 else
                 {
-                    ToMove.transform.position = Vector3.Lerp(positionUp, positionDown, animationProgress / smooth);
+                    {
+                        if (ToMove.transform.position == positionUp)
+                        {
+                            final = !final;
+                            vrai = !vrai;
+                        }
+                        else
+                        {
+                            if (ToMove.transform.position == positionDown && vrai)
+                            {
+                                StartCoroutine(Displacement());
+                                animationProgress = 0.0f;
+                                vrai = !vrai;
+                                ToMove.transform.position = new Vector3(ToMove.transform.position.x + 0.0001f, ToMove.transform.position.y + 0.0001f, ToMove.transform.position.z + 0.0001f);
+                            }
+                            else
+                            {
+                                ToMove.transform.position = Vector3.Lerp(positionDown, positionUp, animationProgress / smooth);
+                            }
+                        }
+                    }
                 }
             }
-        }
-        if (!final)
-        {
-            if (ToMove.transform.position == positionUp)
-            {
-                final = !final;
-            }
-            else
-            {
-                if (ToMove.transform.position == positionDown)
-                {
-                    StartCoroutine(Displacement());
-                    animationProgress = 0.0f;
-                    ToMove.transform.position = new Vector3(ToMove.transform.position.x + 0.0001f, ToMove.transform.position.y + 0.0001f, ToMove.transform.position.z + 0.0001f);
-                }
-                else
-                {
-                    ToMove.transform.position = Vector3.Lerp(positionDown, positionUp, animationProgress / smooth);
-                }
-            }
-        }
-      
-	}
+    }*/
 }
