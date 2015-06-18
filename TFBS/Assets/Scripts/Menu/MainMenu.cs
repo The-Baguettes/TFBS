a58851310826ea7206be4db24b798b0abcc77928
ThinkChaos <ThinkChaos@users.noreply.github.com>
2015-06-18 20:07:45 +0200

    New main menu

--- TFBS/Assets/Scripts/Menu/MainMenu.cs
+++ TFBS/Assets/Scripts/Menu/MainMenu.cs
@@ -1,7 +1,9 @@
 ﻿public class MainMenu : MenuBase
 {
-    protected void Awake()
+    protected override void Start()
     {
+        base.Start();
+
         if (SceneManager.LoadedScene == Scene.MainMenu)
             Show();
     }
