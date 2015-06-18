f3ae11099122772352e353313802451319e5b686
Paultato <Paultato@users.noreply.github.com>
2015-06-18 18:45:49 +0200

    First changes before crash

---
+++ TFBS/Assets/Scripts/Hostage.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hostage : MonoBehaviour
+{
+    Transform Ia;
+    Transform leader;
+    bool followingplayer;
+    bool PlayerIsNear=true;
+    NavMeshAgent navmesh;
+    int rotspeed = 60;
+
+    void Awake()
+    {
+        leader = GameObject.FindGameObjectWithTag(Tags.Player).transform;
+        navmesh = GetComponent<NavMeshAgent>();
+        Ia = transform;
+    }
+
+    void Update()
+    {
+        if ((leader.position.x - Ia.transform.position.x) > 1)
+            followingplayer = true;
+        if (followingplayer && PlayerIsNear)
+            FollowingPlayer();
+    }
+
+    void FollowingPlayer()
+    {
+        while (followingplayer)
+        {
+            if ((leader.position.x - Ia.transform.position.x) <= 1)
+                followingplayer = false;
+            navmesh.SetDestination(leader.transform.position);
+        }
+        if (followingplayer == false) 
+        {
+            Ia.Rotate(0,rotspeed*Time.deltaTime,0);
+        }
+    }
+}
