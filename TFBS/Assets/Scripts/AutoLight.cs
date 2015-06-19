e5d778bc52c88e1f5b6edbac3439f55405841f4f
mickael_au <mickmick93@users.noreply.github.com>
2015-06-19 21:36:42 +0200

    Scamera Camera redLight added

--- TFBS/Assets/Scripts/AutoLight.cs
+++ TFBS/Assets/Scripts/AutoLight.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class AutoLight : MonoBehaviour
 {
-
     bool contact;
     public GameObject Candle1;
     public GameObject Candle2;
--- TFBS/Assets/Scripts/SCamera.cs
+++ TFBS/Assets/Scripts/SCamera.cs
@@ -3,55 +3,70 @@ using System.Collections;
 
 public class SCamera : MonoBehaviour
 {
-    public bool isPlayer;
+   // public bool isPlayer;
     Vector3 minAngle;
     Vector3 maxAngle;
     Vector3 targetAngle;
     public float toRotateLeft;
-   // public float toRotateRight;
+    // public float toRotateRight;
     public float smooth;
     float animationStartTime;
-    public  GameObject ToMove;
+    public GameObject ToMove;
     bool spotted;
     bool final;
     float animationProgress = 0.0f;
-    
+    public GameObject redLight;
     void Start()
     {
         minAngle = ToMove.transform.eulerAngles + new Vector3(0, -toRotateLeft / 2, 0);
         maxAngle = ToMove.transform.eulerAngles + new Vector3(0, toRotateLeft / 2, 0);
         targetAngle = minAngle;
-
+        spotted = false;
     }
 
-    void OnTriggerStay(Collider col)
+    void OnTriggerEnter(Collider col)
     {
         if (col.tag == Tags.Player)
+            spotted = true;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag== Tags.Player && spotted)
         {
-            Alarm();   
+            StartCoroutine(Alarm());
         }
     }
 
-    public void Alarm()
+    IEnumerator Alarm()
     {
-            
+        if (spotted)
+        {
+            for (int i = 0; i < 10; i++)
+            {//sound
+                redLight.SetActive(true);
+                yield return new WaitForSeconds(2f);
+                redLight.SetActive(false);
+                yield return new WaitForSeconds(1f);
+            }
+        }
     }
-    
-    IEnumerator Displacement()
+
+    IEnumerator Displacement(float a)
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(a);
     }
 
 
-    void Update() 
+    void Update()
     {
-        if(Time.time <= animationStartTime)
+        if (Time.time <= animationStartTime)
         {
             return;
         }
-        animationProgress += Time.deltaTime*smooth;
-        ToMove.transform.eulerAngles = Vector3.Lerp(ToMove.transform.eulerAngles, targetAngle,animationProgress);
-        if(animationProgress >= 1)
+        animationProgress += Time.deltaTime * smooth;
+        ToMove.transform.eulerAngles = Vector3.Lerp(ToMove.transform.eulerAngles, targetAngle, animationProgress);
+        if (animationProgress >= 1)
         {
             animationProgress = 0;
             animationStartTime = Time.time + 5f;
@@ -60,5 +75,6 @@ public class SCamera : MonoBehaviour
             else
                 targetAngle = minAngle;
         }
+
     }
 }
