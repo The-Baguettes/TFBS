faa7eb15b55892e84c6da007a0ebd7bbf41418b4
mickael_au <mickmick93@users.noreply.github.com>
2015-06-21 18:52:26 +0200

    New camera 360²²

--- TFBS/Assets/Scripts/Surveillance/Surveillance.cs
+++ TFBS/Assets/Scripts/Surveillance/Surveillance.cs
@@ -2,37 +2,60 @@
 using System.Collections.Generic;
 using System.Collections;
 
-public class Surveillance : MonoBehaviour {
+public class Surveillance : MonoBehaviour
+{
 
     bool isPlayer;
     public GameObject GOCamera;
-    List<GameObject> ListCamera;
+    public Camera Main;
+    List<Camera> ListCamera;
     int compteur;
-	void Start () {
+    int last;
+
+    void Awake()
+    {
         isPlayer = false;
-        compteur = 0; 
-        for (int i = 0; i < GOCamera.transform.childCount; i++)
+        compteur = 0;
+        ListCamera = new List<Camera>();
+        GOCamera.GetComponentsInChildren<Camera>(ListCamera);
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < ListCamera.Count; i++)
         {
-            ListCamera[i] = GOCamera.transform.GetChild(i).GetChild(0).gameObject;
+            ListCamera[i].enabled = false;
+            ListCamera[i].depth = Main.depth + 1;
         }
-	}
+    }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.tag == Tags.Player)
             isPlayer = true;
     }
+
     void OnTriggerExit(Collider col)
     {
         if (col.tag == Tags.Player)
             isPlayer = false;
     }
 
-	void Update () {       
-       if(Input.GetKeyDown(KeyCode.F))
-       {
-           compteur += 1;
-       }
+    void Update()
+    {
+        if (!isPlayer)
+            return;
 
-	}
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ListCamera[compteur].enabled = false;
+            compteur = (compteur + 1) % ListCamera.Count;
+            ListCamera[compteur].enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            ListCamera[compteur].enabled = false;
+        }
+
+    }
 }
