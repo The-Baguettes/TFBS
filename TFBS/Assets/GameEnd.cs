4e81bb52b38d05273a7ecde4f9d8454c66a47910
vhulcroc <cai.richard@hotmail.fr>
2015-06-22 17:47:16 +0200

    Credit scene with scripts

---
+++ TFBS/Assets/GameEnd.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEnd : MonoBehaviour
+{
+    bool contact;
+
+    void Start()
+    {
+        contact = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            contact = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            contact = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (contact)
+        {
+            GUI.Label(new Rect((Screen.width / 2) - 150, Screen.height / 2 - 50, 250, 200), "Press 'F' to put the briefcase");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                SceneManager.LoadScene(Scene.Credit);
+            }
+        }
+    }
+}
---
+++ TFBS/Assets/MovingText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingText : MonoBehaviour
+{
+
+    public Transform text;
+    public float speed;
+    float position;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        }
+        if (text.position.y > -410)
+        {
+            text.position = new Vector3(text.position.x, text.position.y - speed, text.position.z);
+        }
+    }
+}
