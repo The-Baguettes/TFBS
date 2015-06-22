ade47008737c2a085709e54cec720980a49d3e2b
mickael_au <mickmick93@users.noreply.github.com>
2015-06-22 21:32:48 +0200

    Secret base light added + modif autolight.cs

--- TFBS/Assets/Scripts/AutoLight.cs
+++ TFBS/Assets/Scripts/AutoLight.cs
@@ -6,6 +6,7 @@ public class AutoLight : MonoBehaviour
     bool contact;
     public GameObject Candle1;
     public GameObject Candle2;
+    public float speed;
     // Update is called once per frame
     IEnumerator LightSystem()
     {
@@ -13,7 +14,7 @@ public class AutoLight : MonoBehaviour
         {
             Candle1.transform.GetChild(i).gameObject.SetActive(true);
             Candle2.transform.GetChild(i).gameObject.SetActive(true);
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(speed);
         }
 
     }
